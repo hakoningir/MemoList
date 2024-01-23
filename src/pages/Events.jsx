87337ea07d1740b1760de453b20361eb0a3fae1d
hakoningir <hir12@hi.is>
2024-01-23 20:45:19 +0000

    delete button is trascan logo and has hover

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,71 +1,82 @@
 import { useEffect, useState } from "react";
 import './Events.css';
+import trashcan from './../trashcan.png'
 function Event() {
-    const [inputTitle, setInputTitle] = useState('');
-    const [inputDescription, setInputDescription] = useState('');
-    const [data, setData] = useState([]);
+  const [inputTitle, setInputTitle] = useState('');
+  const [inputDescription, setInputDescription] = useState('');
+  const [data, setData] = useState([]);
+  const [titlemsg, setTitlemsg] = useState('');
+  const [descriptionmsg, setDescriptionmsg] = useState('');
+  useEffect(() => {
+    data.length > 0 && window.localStorage.setItem("event", JSON.stringify(data));
+  }, [data]);
 
-    useEffect(() => {
-        data.length > 0 && window.localStorage.setItem("event", JSON.stringify(data));
-    }, [data]);
+  useEffect(() => {
+    const storedData = JSON.parse(window.localStorage.getItem("event"));
+    if (!!storedData) setData(storedData);
+  }, []);
 
-    useEffect(() => {
-        const storedData = JSON.parse(window.localStorage.getItem("event"));
-        if (!!storedData) setData(storedData);
-    }, []);
-
-    function HandleSubmit(e) {
-        e.preventDefault();
-        let submitted = [inputTitle, inputDescription]
-        submitted && setData([...data, submitted]);
-        setInputTitle('');
-        setInputDescription('');
+  function HandleSubmit(e) {
+    e.preventDefault();
+    // Cannot create an event without title and/or description
+    if (inputTitle.length > 0 && inputDescription.length > 0) {
+      let submitted = [inputTitle, inputDescription];
+      submitted && setData([...data, submitted]);
+      setInputTitle('');
+      setInputDescription('');
+      setTitlemsg('');
+      setDescriptionmsg('');
     }
+    if (inputTitle.length === 0) setTitlemsg("Must provide title");
+    if (inputDescription.length === 0) setDescriptionmsg("Must provide description");
+    console.log(titlemsg, descriptionmsg);
+  }
+  function deleteEvent(key) {
+    setData(data.filter((_, index) => index !== key));
+  }
 
-    function deleteEvent(key) {
-        setData(data.filter((_, index) => index !== key));
-      }
-    
-      return (
-        <>
-          <form onSubmit={HandleSubmit}>
-            <div>
-              <div>
-                <input
-                  type="title"
-                  placeholder="Event title"
-                  value={inputTitle}
-                  onChange={(e) => setInputTitle(e.target.value)}
-                />
-              </div>
-              <div>
-                <textarea
-                  type="description"
-                  placeholder="Event description"
-                  value={inputDescription}
-                  onChange={(e) => setInputDescription(e.target.value)}
-                ></textarea>
-              </div>
-            </div>
-            <div>
-              <button type="submit">Create event</button>
-            </div>
-          </form>
+  return (
+    <>
+      <form onSubmit={HandleSubmit}>
+        <div>
           <div>
-            <ul className="card-container">
-              {data.map((item, ke) => (
-                <div key={ke} className="card">
-                  <h1>{item[0]}</h1>
-                  <p>{item[1]}</p>
-                  <button type="delete" value={ke} onClick={() => deleteEvent(ke)}>
-                    Delete event
-                  </button>
-                </div>
-              ))}
-            </ul>
+            {titlemsg && <p className="titlemsg">{titlemsg}</p>}
+            <input
+              type="title"
+              placeholder="Event title"
+              value={inputTitle}
+              onChange={(e) => setInputTitle(e.target.value)}
+            />
           </div>
-        </>
-      );
+          <div>
+            {descriptionmsg && <p className="descriptionmsg">{descriptionmsg}</p>}
+            <textarea
+              type="description"
+              placeholder="Event description"
+              value={inputDescription}
+              onChange={(e) => setInputDescription(e.target.value)}
+            ></textarea>
+          </div>
+        </div>
+        <div>
+          <button type="submit">Create event</button>
+        </div>
+      </form>
+      <div>
+        <ul className="card-container">
+          {data.map((item, ke) => (
+            <div key={ke} className="card">
+              <h1>{item[0]}</h1>
+              <p>{item[1]}</p>
+              <button type="delete" value={ke} onClick={() => deleteEvent(ke)} >
+                <img src={trashcan} alt=""/>
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Event;
